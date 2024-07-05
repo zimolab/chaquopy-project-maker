009682720e7d621795296ca332ee3d5a194a7fee
zimolab <zimolab@aliyun.com>
2024-07-06 00:44:40 +0800

    feat(main): add app menus

--- chaquopy_project_maker/main.py
+++ chaquopy_project_maker/main.py
@@ -1,5 +1,7 @@
 from PyQt6.QtWidgets import QApplication
 from pyguiadapter import GUIAdapter, DocumentFormat
+from pyguiadapter.ui import ActionItem, ExecutionContext
+from pyguiadapter.interact.upopup import show_about_popup
 
 from chaquopy_project_maker.core import make_project
 from chaquopy_project_maker.configs import CONFIGS
@@ -25,6 +27,73 @@ for more information.
 
 """
 
+EXTRA_PLUGIN_MAVEN_REPOS_ALIYUN = [
+    "https://maven.aliyun.com/repository/google",
+    "https://maven.aliyun.com/repository/central",
+    "https://maven.aliyun.com/repository/gradle-plugin",
+]
+
+EXTRA_DEP_MAVEN_REPOS_ALIYUN = [
+    "https://maven.aliyun.com/repository/google",
+    "https://maven.aliyun.com/repository/central",
+]
+
+
+def on_add_aliyun_repositories(ctx: ExecutionContext):
+    try:
+        # add aliyun's repos to extra_plugin_maven_repositories and extra_dependency_maven_repositories
+        extra_plugin_maven_repositories = (
+            ctx.get_param_value("extra_plugin_maven_repositories") or []
+        )
+        extra_dependency_maven_repositories = (
+            ctx.get_param_value("extra_dependency_maven_repositories") or []
+        )
+        for repo in EXTRA_PLUGIN_MAVEN_REPOS_ALIYUN:
+            if repo not in extra_plugin_maven_repositories:
+                extra_plugin_maven_repositories.append(repo)
+        for repo in EXTRA_DEP_MAVEN_REPOS_ALIYUN:
+            if repo not in extra_dependency_maven_repositories:
+                extra_dependency_maven_repositories.append(repo)
+        ctx.set_param_value(
+            "extra_plugin_maven_repositories", extra_plugin_maven_repositories
+        )
+        ctx.set_param_value(
+            "extra_dependency_maven_repositories", extra_dependency_maven_repositories
+        )
+    except BaseException as e:
+        print(e)
+        ctx.logging_fatal(f"{e}")
+        return
+
+
+MENUS = {
+    tr("File"): {
+        "add_aliyun_repositories": ActionItem(
+            text=tr("Add Aliyun Repositories"),
+            callback=on_add_aliyun_repositories,
+        ),
+        "about": ActionItem(
+            text=tr("About"),
+            callback=lambda ctx: show_about_popup(
+                app_name=APP_NAME,
+                app_description=tr(
+                    "Chaquopy Project Maker is a GUI tool to help you create a Chaquopy-integrated Android app project"
+                    "easily. It can save you a lot of time from the repeating and tedious work."
+                ),
+                app_copyright=tr("copyright © zimolab. All right reserved"),
+                app_fields={
+                    tr("License"): "GPL",
+                    tr("Version"): "0.0.1",
+                    tr("Author"): "zimolab",
+                    tr(
+                        "GitHub"
+                    ): "<a href='https://github.com/zimolab/chaquopy-project-maker'>chaquopy-project-maker</a>",
+                },
+            ),
+        ),
+    },
+}
+
 
 def setup(adapter: GUIAdapter) -> GUIAdapter:
     adapter.execution_window_config.title = APP_NAME
@@ -36,6 +105,7 @@ def setup(adapter: GUIAdapter) -> GUIAdapter:
     adapter.execution_window_config.show_func_result_dialog = False
     adapter.execution_window_config.func_error_msg = "{}"
     adapter.execution_window_config.func_error_dialog_title = tr("Error")
+    adapter.execution_window_config.enable_menubar_actions = True
 
     return adapter
 
@@ -47,6 +117,7 @@ def main():
         display_document=APP_DOCUMENTATION,
         document_format=DocumentFormat.MARKDOWN,
         widget_configs=CONFIGS,
+        menus=MENUS,
     )
     gui_adapter.run()
 
